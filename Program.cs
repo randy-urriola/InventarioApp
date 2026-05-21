91cd3e7fc4d434a47aa5118e004003d40b0eb736
randy-urriola <randy.urriolah@gmail.com>
2026-05-21 12:01:07 -0500

    Funciones, modo interactivo con argumentos  y orden inicial del código

--- Program.cs
+++ Program.cs
@@ -9,14 +9,41 @@ using System.Reflection;
 var assembly = Assembly.GetExecutingAssembly();
 var version = assembly.GetName().Version;
 
-Console.WriteLine("==========================================");
-Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
-Console.WriteLine("==========================================");
-Console.WriteLine();
-Console.WriteLine($"Versión: {version}");
-Console.WriteLine($"Plataforma: {Environment.OSVersion}");
-Console.WriteLine($".NET Version: {Environment.Version}");
-Console.WriteLine();
+if (args.Length > 0)
+{
+  switch (args[0].ToLower())
+  {
+    case "--help":
+      MostrarAyuda();
+      Environment.Exit(0);
+      break;
+
+    case "--version":
+      Console.WriteLine($"InventarioApp v[{version}]");
+      Environment.Exit(0); // ejecución exitosa
+      break;
+
+    default:
+      Console.WriteLine($"Error: Comando desconocido '{args[0]}'");
+      Console.WriteLine("Use --help para ver los comandos disponibles.");
+      Environment.Exit(2); // error de argumentos
+      break;
+  }
+}
+
+MostrarBanner();
+
+// Modo interactivo si no hay argumentos
+Console.Write("Ingrese un comando (o 'salir' para terminar): ");
+string? entrada = Console.ReadLine(); // STDIN, con el ? acepta entradas nulas
+
+if (string.IsNullOrWhiteSpace(entrada) || entrada.ToLower() == "salir")
+{
+  Console.WriteLine("Hasta luego!"); // STDOut para mostrar salida
+  Environment.Exit(0);
+}
+
+/*
 Console.WriteLine("Estructura del proyecto:");
 Console.WriteLine("  InventarioApp/");
 Console.WriteLine("   |-- Program.cs");
@@ -29,4 +56,31 @@ Console.WriteLine("Configuración .csproj");
 Console.WriteLine("Carpeta src/ creada");
 Console.WriteLine("Metadatos configurados");
 Console.WriteLine();
-Console.WriteLine("Próximo paso: Checkpoint");
+Console.WriteLine("Próximo paso: Checkpoint"); */
+
+// ====================== Funciones ======================
+
+void MostrarBanner()
+{
+  Console.WriteLine("==========================================");
+Console.WriteLine("    SISTEMA DE GESTIÓN DE INVENTARIO      ");
+Console.WriteLine("==========================================");
+Console.WriteLine();
+Console.WriteLine($"Versión: {version}");
+Console.WriteLine($"Plataforma: {Environment.OSVersion}");
+Console.WriteLine($".NET Version: {Environment.Version}");
+Console.WriteLine();
+}
+
+void MostrarAyuda()
+{
+  Console.WriteLine("USO: InventarioApp [comando] [opciones]");
+    Console.WriteLine();
+    Console.WriteLine("COMANDOS:");
+    Console.WriteLine("  --help, -h      Muestra esta ayuda");
+    Console.WriteLine("  --version, -v   Muestra la version del programa");
+    Console.WriteLine();
+    Console.WriteLine("EJEMPLOS:");
+    Console.WriteLine(" dotnet run -- --help");
+    Console.WriteLine(" dotnet run -- --version");
+}
